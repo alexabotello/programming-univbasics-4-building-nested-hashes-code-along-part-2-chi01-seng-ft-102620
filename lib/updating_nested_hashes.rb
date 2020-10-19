c8a48d81ab35a12c28884df8c1e87fa0339ab4b8
Alexa Botello <abotello20@yahoo.com>
2020-10-19 17:38:02 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -39,7 +39,7 @@ def update_status
       {name: "Steven", age: "30", attitude: "confused"},
       {name: "Nurse", age: "44", attitude: "worried"}
       ]
-  }
+  }}
   epic_tragedy[:montague][:hero][:status] = "dead"
   epic_tragedy[:capulet][:heroine][:status] = "dead"
   epic_tragedy
